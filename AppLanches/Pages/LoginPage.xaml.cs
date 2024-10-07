9c9e2b7dc400cdea9d4c1877906c46c92fd9ee0d
SalavessaMota <nunosalavessa@hotmail.com>
2024-10-07 15:13:27 +0100

    Add login and registration pages with navigation
    - Modified `App.xaml.cs` to include `ApiService` and set `MainPage` to a `NavigationPage` containing `RegisterPage`.
    - Updated `AppLanches.csproj` to include `LoginPage.xaml` and `RegisterPage.xaml` for compilation.
    - Updated `MauiProgram.cs` to add `ApiService` as a singleton service.
    - Added `LoginPage.xaml` for the login UI with email, password fields, login button, and navigation link to registration.
    - Added `LoginPage.xaml.cs` for login logic, handling button click and navigation.
    - Added `RegisterPage.xaml` for the registration UI with fields for name, email, phone, password, registration button, and navigation link to login.
    - Added `RegisterPage.xaml.cs` for registration logic, handling button click and navigation.

---
+++ AppLanches/Pages/LoginPage.xaml.cs
@@ -0,0 +1,45 @@
+using AppLanches.Services;
+
+namespace AppLanches.Pages;
+
+public partial class LoginPage : ContentPage
+{
+    private readonly ApiService _apiService;
+
+    public LoginPage(ApiService apiService)
+    {
+        InitializeComponent();
+        _apiService = apiService;
+    }
+
+    private async void BtnSignIn_Clicked(object sender, EventArgs e)
+    {
+        if (string.IsNullOrEmpty(EntEmail.Text))
+        {
+            await DisplayAlert("Error", "Insert your email", "Cancel");
+            return;
+        }
+
+        if (string.IsNullOrEmpty(EntPassword.Text))
+        {
+            await DisplayAlert("Error", "Insert your password", "Cancel");
+            return;
+        }
+
+        var response = await _apiService.Login(EntEmail.Text, EntPassword.Text);
+
+        if (!response.HasError)
+        {
+            Application.Current!.MainPage = new AppShell();
+        }
+        else
+        {
+            await DisplayAlert("Error", "Something went wrong!!!", "Cancel");
+        }
+    }
+
+    private async void TapRegister_TappedAsync(object sender, TappedEventArgs e)
+    {
+        await Navigation.PushAsync(new RegisterPage(_apiService));
+    }
+}
---
+++ AppLanches/Pages/RegisterPage.xaml.cs
@@ -0,0 +1,35 @@
+using AppLanches.Services;
+
+namespace AppLanches.Pages;
+
+public partial class RegisterPage : ContentPage
+{
+    private readonly ApiService _apiService;
+
+    public RegisterPage(ApiService apiService)
+	{
+		InitializeComponent();
+        _apiService = apiService;
+    }
+
+    private async void BtnSignup_ClickedAsync(object sender, EventArgs e)
+    {
+        var response = await _apiService.RegisterUser(EntNome.Text, EntEmail.Text,
+                                                          EntPhone.Text, EntPassword.Text);
+
+        if (!response.HasError)
+        {
+            await DisplayAlert("Warning", "Account registered successfully !!", "OK");
+            await Navigation.PushAsync(new LoginPage(_apiService));
+        }
+        else
+        {
+            await DisplayAlert("Error", "Something went wrong!!!", "Cancel");
+        }
+    }
+
+    private async void TapLogin_TappedAsync(object sender, TappedEventArgs e)
+    {
+        await Navigation.PushAsync(new LoginPage(_apiService));
+    }
+}
